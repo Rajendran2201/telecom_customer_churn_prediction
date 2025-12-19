b73281b8a3d0bc04430ab0f39e6a0aed057c2178
Raj <asrajendrayadav@gmail.com>
2025-12-19 14:15:28 +0530

    feature engineering completed

--- telecom_customer_churn_prediction/dataset.py
+++ telecom_customer_churn_prediction/dataset.py
@@ -12,7 +12,7 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
+    input_path: Path = RAW_DATA_DIR / "customer_churn.csv",
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
